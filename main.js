e2047f90ad79822c152c8156b66e43758c7f0d49
Josh <joshuawen97@gmail.com>
2021-07-30 12:20:41 +0700

    javascript added

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-// alert(`working`);
+console.log('working');
+
+const billAmount = document.getElementById('billAmount');
+////////////////////////////////////////////////////////////////////////
+const fivePercent = document.getElementById('fivePercent');
+const tenPercent = document.getElementById('tenPercent');
+const fifteenPercent = document.getElementById('fifteenPercent');
+const twentyFivePercent = document.getElementById('twentyFivePercent');
+const fiftyPercent = document.getElementById('fiftyPercent');
+const custom = document.getElementById('custom');
+////////////////////////////////////////////////////////////////////////
+const numberOfPeople = document.getElementById('numberOfPeopleInput');
